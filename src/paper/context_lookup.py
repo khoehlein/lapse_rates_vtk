ff5a910f17c436241046f704a334597846055924
khoehlein <kevin.hoehlein@tum.de>
2024-03-20 14:54:30 +0100

    start config lookup

--- src/paper/context_lookup.py
+++ src/paper/context_lookup.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 import pandas as pd
+import xarray as xr
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config-file', type=str, required=True)
@@ -12,6 +13,9 @@ with open(args['config_file'], 'r') as f:
 
 station_obs = pd.read_parquet(configs['stations']['observations'])
 station_preds = pd.read_parquet(configs['stations']['predictions'])
-station_meta = pd.read_parquet(configs['stations']['meta_data'])
+station_meta = pd.read_csv(configs['stations']['meta_data'], index_col=0)
+orography = xr.open_dataset(configs['grid_data']['o1280']['z']['path'])[configs['grid_data']['o1280']['z']['key']]
 
+lookup = NearestNeighbors()
 
+print('Done')
